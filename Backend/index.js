dc7b519d1f20bca772872e955f1d7ba4d3f4e171
omkar <omkarmore0702@gmail.com>
2025-01-06 20:06:49 +0530

    Version 0.0.0.1

--- Backend/index.js
+++ Backend/index.js
@@ -2,22 +2,37 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.js";
 
 dotenv.config();
+
 const app = express();
-app.use(cors({
+
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-  }));
+  })
+);
+
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allowed us json incomming request
-app.use(cookieParser());//allow us to get incomming cookies
+app.use(cookieParser()); //allow us to get incomming cookies
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
